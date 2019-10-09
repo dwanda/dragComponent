ba4e69dd92abce9d328f3f61eaad6aadfaee0c1e
ddwan <huangyuda13@qq.com>
2019-10-09 17:22:27 +0800

    1.先创建组件架构

---
+++ examples/components/example1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'example1',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ packages/card-dragger/index.js
@@ -0,0 +1,11 @@
+
+// 导入组件，组件必须声明 name
+// import colorPicker from './src/color-picker.vue'
+
+// 为组件提供 install 安装方法，供按需引入
+// colorPicker.install = function (Vue) {
+//   Vue.component(colorPicker.name, colorPicker)
+// }
+
+// 默认导出组件
+// export default colorPicker
